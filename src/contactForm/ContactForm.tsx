669acd6f0d0134ab4d5edfc603fd774544d12994
PavelVoitov <voitov.pavel@yandex.ru>
2023-03-12 23:00:48 +0300

    fix: added the ability to send a message to the email

--- src/contactForm/ContactForm.tsx
+++ src/contactForm/ContactForm.tsx
@@ -1,24 +1,45 @@
-import React from "react";
+import React, {FormEvent} from "react";
 import styleContainer from '../common/styles/Container.module.scss'
 import s from './ContactForm.module.scss'
 import {Button} from "../common/components/button/Button";
 import {Title} from "../common/components/title/Title";
+import axios from "axios";
 
 
 export const ContactForm = () => {
-    return (
-        <div id={'contactForm'} className={s.contactsBlock}>
-            <div className={`${styleContainer.container} ${s.contactsContainer}`}>
-                <Title title={'contacts'}/>
-                <div className={s.formBlock}>
-                    <form className={s.form} action="">
-                        <input type='text' placeholder='Name'/>
-                        <input type='text' placeholder='Email'/>
-                        <textarea placeholder='Your massage...'/>
-                        <Button type={'submit'} title={'Send'}/>
-                    </form>
-                </div>
-            </div>
-        </div>
-    )
+
+	const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+		e.preventDefault()
+		const data = e.currentTarget as typeof e.currentTarget & {
+			name: { value: string };
+			email: { value: string };
+			message: { value: string };
+		};
+		const formData = {
+			name: data.name.value,
+			email: data.email.value,
+			message: data.message.value
+		}
+		console.log(formData)
+		axios.post('https://portfolio-gmail-smtp-topaz.vercel.app/sendMessage', formData)
+			.then(() => {
+				alert('Ready!')
+			})
+	}
+
+	return (
+		<div id={'contactForm'} className={s.contactsBlock}>
+			<div className={`${styleContainer.container} ${s.contactsContainer}`}>
+				<Title title={'contacts'}/>
+				<div className={s.formBlock}>
+					<form className={s.form} onSubmit={handleSubmit}>
+						<input type='text' placeholder='Name' name={'name'}/>
+						<input type='text' placeholder='Email' name={'email'}/>
+						<textarea placeholder='Your massage...' name={'message'}/>
+						<Button type={'submit'} title={'Send'}/>
+					</form>
+				</div>
+			</div>
+		</div>
+	)
 }
