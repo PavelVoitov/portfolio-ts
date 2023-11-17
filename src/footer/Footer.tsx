93fc2e23f5bef1f8460cfe7a9da48df77082619d
PavelVoitov <voitov.pavel@yandex.ru>
2023-11-17 22:14:06 +0300

    add: const for current year fix: footer text

--- src/footer/Footer.tsx
+++ src/footer/Footer.tsx
@@ -9,28 +9,28 @@ import ReactTypingEffect from "react-typing-effect";
 
 
 export const Footer = () => {
-    const currentYear = new Date().getFullYear()
+	const currentYear = new Date().getFullYear()
 
-    return (
-      <div className={s.footer}>
-          <div className={s.container}>
-              <Title title={'Pavel Voitov'}/>
-              <div className={s.iconsContainer}>
-                  <a href="https://t.me/voitov_pavel"><img src={telegram} alt="telegram"/></a>
-                  <a href="https://www.facebook.com/profile.php?id=100009416962471"><img src={facebook} alt="facebook"/></a>
-                  <a href="https://instagram.com/voitov_pavel?igshid=ZDdkNTZiNTM="><img src={instagram}
-                                                                                        alt="instagram"/></a>
-                  <a href="https://www.linkedin.com/in/pavel-voitov-200125100/"><img src={linkedin} alt="linkedin"/></a>
-              </div>
-              <div className={s.span}>
-                  <ReactTypingEffect text={'Created by Pavel Voitov'}
-                                     speed={100}
-                                     className={s.reactTypingEffect}
-                                     staticText={`© ${currentYear}`}
-                                     eraseSpeed={100}
-                  />
-              </div>
-          </div>
-      </div>
-    )
+	return (
+		<div className={s.footer}>
+			<div className={s.container}>
+				<Title title={'Pavel Voitov'}/>
+				<div className={s.iconsContainer}>
+					<a href="https://t.me/voitov_pavel"><img src={telegram} alt="telegram"/></a>
+					<a href="https://www.facebook.com/profile.php?id=100009416962471"><img src={facebook} alt="facebook"/></a>
+					<a href="https://instagram.com/voitov_pavel?igshid=ZDdkNTZiNTM="><img src={instagram}
+																																								alt="instagram"/></a>
+					<a href="https://www.linkedin.com/in/pavel-voitov-200125100/"><img src={linkedin} alt="linkedin"/></a>
+				</div>
+				<div className={s.span}>
+					<ReactTypingEffect text={'Created by Pavel Voitov'}
+														 speed={100}
+														 className={s.reactTypingEffect}
+														 staticText={`© ${currentYear}`}
+														 eraseSpeed={100}
+					/>
+				</div>
+			</div>
+		</div>
+	)
 }
