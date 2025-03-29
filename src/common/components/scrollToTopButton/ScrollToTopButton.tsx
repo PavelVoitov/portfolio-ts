24bfcbdd90bc4276cf69a6f7b81d48e011dfab37
PavelVoitov <voitov.pavel@yandex.ru>
2025-03-30 01:00:24 +0300

    change Button styles

--- src/common/components/scrollToTopButton/ScrollToTopButton.tsx
+++ src/common/components/scrollToTopButton/ScrollToTopButton.tsx
@@ -1,21 +1,22 @@
 import s from './ScrollToTopButton.module.scss'
 import { FaAngleUp } from "react-icons/fa";
 import {useEffect, useState} from "react";
-import {animateScroll} from "react-scroll";
 
 export const ScrollToTop = () => {
 	const [showTopButton, setShowTopButton] = useState(false);
 	useEffect(() => {
-		window.addEventListener("scroll", () => {
-			if (window.scrollY > 600) {
-				setShowTopButton(true);
-			} else {
-				setShowTopButton(false);
-			}
-		});
+		const handleScroll = () => {
+			setShowTopButton(window.scrollY > 600);
+		};
+
+		window.addEventListener("scroll", handleScroll);
+		return () => window.removeEventListener("scroll", handleScroll);
 	}, []);
 	const goToTop = () => {
-		animateScroll.scrollToTop()
+		window.scrollTo({
+			top: 0,
+			behavior: "smooth",
+		})
 	};
 	return (
 		<div className={s.top_to_btm}>
